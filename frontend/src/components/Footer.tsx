00bb4f5c763c7a95a6210daa9a3164b0af79d966
Jessica Roussy <jroussy.dev@gmail.com>
2026-06-13 01:58:07 +0200

    Redisign Footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -2,25 +2,36 @@ import Logo from "./Logo";
 
 export default function Footer() {
   return (
-    <footer className="bg-[#020617] border-t border-slate-200 mt-16 text-slate-300">
+    <footer className="bg-[#020617] border-t border-slate-200 text-slate-300">
 
 {/* Bloc principal */}
-      <div className="max-w-7xl mx-auto px-6 py-16">
+      <div className="max-w-7xl mx-auto px-0 py-6">
 {/* Colonnes */}
         <div className="grid gap-10 md:grid-cols-2 lg:grid-cols-4">
           {/* Bloc Jess tech */}
           <div className="">
-            <div className="flex items-center gap-2">
-              < Logo/>
-              Jess Tech
-            </div>
-            <div>
+        <div className="flex items-center gap-8">
+          <div className="text-3xl font-black text-white">
+            <Logo />
+          </div>
+
+          <div>
+            <h1 className="text-xl font-bold text-white">
+              JESS <span className="text-[#0D5BFF]">TECH</span>
+            </h1>
+
+            <p className="text-xs text-slate-300">
+              Développement web & assistante technique
+            </p>
+          </div>
+        </div>
+            <div className="pt-6">
               Des solutions web claires,
               humaines et bien construites.
             </div>
           </div>
           {/* Bloc navigation */}
-          <div className="grid grid-cols-1 gap-4">
+          <div className="grid grid-cols-1 gap-4 ml-26">
             <h1 className="font-bold">Navigation</h1>
             <div className="flex flex-col space-y-1">
             <span>Accueil</span>
@@ -31,7 +42,7 @@ export default function Footer() {
             </div>
           </div>
           {/* Bloc services */}
-          <div className="grid grid-cols-1 gap-4">
+          <div className="grid grid-cols-1 gap-4 ml-18">
             <h1 className="font-bold">Services</h1>
             <div className="flex flex-col space-y-1 -mt-8">
               <span></span>Sites vitrines
@@ -41,7 +52,7 @@ export default function Footer() {
             </div>
           </div>
           {/* Bloc contact */}
-          <div className="grid grid-cols-1 gap-4">
+          <div className="grid grid-cols-1 gap-4 ml-20">
             <h1 className="font-bold">Contact</h1>
             <div className="flex flex-col space-y-1 -mt-10">
               <div className="flex items-center gap-2">
@@ -67,13 +78,16 @@ export default function Footer() {
           </div>
         </div>
         {/* Barre du bas */}
-        <div className="border-t border-slate-800 mt-12 pt-6">
-          © 2026 Jess Tech
-          Tous droits réservés
-
-          Mentions légales
-          Politique de confidentialité
+        <div className="border-t border-slate-800 mt-8 pt-6">
+          <div className="flex items-center gap-155">
+          <div>
+          © 2026 Jess Tech - Tous droits réservés
+</div>
+<div>
+          <a href="">Mentions légales</a> | <a href="">Politique de confidentialité</a>
         </div>
+        </div>
+      </div>
       </div>
     </footer>
   )
